0318c71efb14c0b110ff0d41eea9570a9157b4cc
deman9 <andem@tuta.io>
2024-09-30 13:09:33 +0200

    work in progress

--- home.nix
+++ home.nix
@@ -70,9 +70,19 @@ programs.git = {
   };
 
 programs.tmux = {
+    keyMode = "vi";
     enable = true;
-    sensibleOnTop = false;
-
+    prefix = "C-Space";
+    baseIndex = 1;
+    sensibleOnTop = true;
+    plugins = with pkgs; [
+      tmuxPlugins.nord
+    ];
+    extraConfig = ''
+      set -g @plugin nord
+      set -g renumber-windows on
+      set -g status-position bottom
+    '';
   };
 
 
